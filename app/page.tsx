56d28940812542db5c0adeea4186922653db7fb8
hixb <Hello16369@163.com>
2024-05-12 21:29:48 +0800

    style: responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -52,8 +52,8 @@ export default function Home() {
   }
 
   return (
-    <main className={clsx('w-11/12 mx-auto mt-20 flex justify-between pb-120')}>
-      <div className={clsx('shadow-[0_10px_50px_rgba(0,0,0,.1)] w-3/5 p-30 rounded-2xl bg-[var(--my-box-bg)]')}>
+    <main className={clsx('w-11/12 mx-auto mt-20 flex justify-between pb-120 max-md:flex-col')}>
+      <div className={clsx('shadow-[0_10px_50px_rgba(0,0,0,.1)] w-3/5 p-30 rounded-2xl bg-[var(--my-box-bg)] max-md:w-full')}>
         <h3 className={clsx('text-xl !font-bold')}>实时动态</h3>
         <div className={clsx('mt-20 flex items-center justify-between')}>
           <div className={clsx('px-15 py-10 shadow-[0_10px_50px_rgba(0,0,0,.1)] w-max rounded-2xl')}>
@@ -78,7 +78,7 @@ export default function Home() {
         </div>
         <UserList />
       </div>
-      <div className={clsx('w-[38%]')}>
+      <div className={clsx('w-[38%] max-md:w-full max-md:mt-30')}>
         <div className={clsx('shadow-[0_10px_50px_rgba(0,0,0,.1)] p-30 rounded-2xl h-max bg-[var(--my-box-bg)]')}>
           <h3 className={clsx('text-xl !font-bold')}>名额查询</h3>
           <div className={clsx('mt-20')}>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,32 @@ export default <Config>{
 
       return map
     }, {}),
+    screens: {
+      // => @media (max-width: 1535px) { ... }
+      'max-2xl': { max: '1535px' },
+      // => @media (max-width: 1279px) { ... }
+      'max-xl': { max: '1279px' },
+      // => @media (max-width: 1023px) { ... }
+      'max-lg': { max: '1023px' },
+      // => @media (max-width: 895px) { ... }
+      'max-2md': { max: '895px' },
+      // => @media (max-width: 767px) { ... }
+      'max-md': { max: '767px' },
+      // => @media (max-width: 639px) { ... }
+      'max-sm': { max: '639px' },
+      // => @media (max-width: 500px) { ... }
+      'max-xs': { max: '500px' },
+      // => @media (min-width: 1535px) { ... }
+      'min-2xl': { min: '1535px' },
+      // => @media (min-width: 1279px) { ... }
+      'min-xl': { min: '1279px' },
+      // => @media (min-width: 1023px) { ... }
+      'min-lg': { min: '1023px' },
+      // => @media (min-width: 767px) { ... }
+      'min-md': { min: '767px' },
+      // => @media (min-width: 639px) { ... }
+      'min-sm': { min: '639px' },
+    },
   },
   plugins: [],
 }
